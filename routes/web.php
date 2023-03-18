ad33cab2b95f5e30283baf31b1945bbdbcce3ab5
ekopram <ekopram9@yahoo.co.id>
2023-03-18 18:15:26 +0700

    view login + register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,4 @@ Route::get('kategori', function () {
 });
 
 Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
